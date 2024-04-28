6088912ab3a884a87d9042f983aff9c4312aae15
Punsath <punsath.20221072@iit.ac.lk>
2024-04-28 17:57:05 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
+import Footer from './Footer/Footer';
+
+const App = () => {
+  return (
+    <Router>
+      <Footer/>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/Footer/Footer.jsx
@@ -0,0 +1,152 @@
+import React from 'react';
+import './footer.css';
+import footer from '../assets/footer/logo.png';
+import icon from '../assets/footer/icon.png';
+
+const Footer = () => {
+  return (
+    <div className='all'>
+    <div className="container">
+      <footer className="py-5">
+        <div className="row align-items-center">
+
+    
+
+
+  <div className="col-md-2 mb-3">
+    <h5>WHO WE HELP</h5><br />
+    <ul className="nav flex-column">
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0  unhide">Enterprise</a></li>
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Hospitals</a></li>
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Aged Care</a></li>
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Clinical Customers</a></li>
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Management</a></li>
+      <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">IT & Operations</a></li>
+
+      <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+      <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+      <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+    </ul>
+  </div>
+          
+          
+
+          <div className="col-md-2 mb-3">
+            <h5>PRODUCTS & SOLUTIONS</h5><br />
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide ">Concentric Care Platform </a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Responder</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Reach</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Safety Monitoring</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Concierge</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Insight</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Pinpoint</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Lighthouse</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Vantage</a></li>
+            </ul>
+          </div>
+          <div className="col-md-2 mb-3">
+            <h5>ABOUT US</h5><br />
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Our Cpmpany</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Our Team</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Work at Rauland</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide ">Careers</a></li>
+
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+             
+            </ul>
+          </div>
+          <div className="col-md-2 mb-3">
+            <h5>RESOURCES</h5><br />
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">News</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Projects</a></li>
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Case Studies</a></li>
+
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              
+            </ul>
+          </div>
+          <div className="col-md-2 mb-3">
+            <h5>SUPPORT</h5><br />
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Services & Support</a></li>
+
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+            </ul>
+          </div>
+
+          <div className="col-md-2 mb-3">
+            <h5>CONTACT US</h5><br />
+            <ul className="nav flex-column">
+              <li className="nav-item mb-2"><a href="#" className="nav-link p-0 unhide">Login</a></li>
+
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+              <li className="nav-item mb-2"><a href="#" id="hide" className="nav-link p-0 hide">hide</a></li>
+            </ul>
+          </div>
+
+          <div className="col-md-2 mb-3">
+    <ul className="nav flex-column">
+      <button className="btn btn-primary mb-2 custom-button">
+        SUBSCRIBE
+        <img src={icon} alt="Icon" className="icon-img" />
+      </button>
+      <br />
+      <p>We acknowledge the Traditional Owners of Country throughout Australia where we work and live and pay our respects to Elders past and present</p>
+      <img className="nav-item mb-2 align-left" src={footer} alt="" /> {/* Added align-left class */}
+    </ul>
+  </div>
+   
+        </div>
+
+        <div className="row">
+  <div className="col-md-0 mb-3">
+    <div className="d-flex flex-column flex-sm-row justify-content-between py-4 my-4 border-top">
+      <div>
+        <p>1300 142 142</p>
+        <p>contact©rauland.com.au <span class="linkedin-text">Linkedin</span></p>
+      </div>
+
+      <ul className="list-unstyled d-flex">
+  <li className="ms-3"><a className="link-body-emphasis custom-link" href="#">Terms & Conditions</a></li>
+  <li className="ms-3"><a className="link-body-emphasis custom-link" href="#">Privacy Policy</a></li>
+  <li className="ms-3"><a className="link-body-emphasis custom-link" href="#">©2023 Rauland Australia</a></li>
+</ul>
+
+
+    </div>
+  </div>
+</div>
+
+      </footer>
+    </div>
+
+    </div>
+  );
+};
+
+export default Footer;
